ee025e925343c01c241c52d5e4693a643c3be1f1
mhammeDev <mhammedvipp@gmail.com>
2023-10-10 14:50:46 +0200

    Initialisation du projet + Création classe model.task

---
+++ to-do/src/main/java/com/todo/list/Model/TaskPriority.java
@@ -0,0 +1,19 @@
+package com.todo.list.Model;
+
+public enum TaskPriority{
+    Weak("Weak"),
+    Average("Average"),
+    High("High");
+
+    private final String label;
+
+    TaskPriority(String label){
+        this.label = label;
+    }
+
+    public String getLabel(){
+        return label;
+    }
+}
+
+
---
+++ to-do/src/main/java/com/todo/list/Model/TaskState.java
@@ -0,0 +1,23 @@
+package com.todo.list.Model;
+
+public enum TaskState {
+    TODO("To do"),
+    IN_PROGRESS("In progress"),
+    COMPLETED("Completed"),
+    POSTPONED("Post poned"),
+    CANCELED("Canceled"),
+    ON_HOLD("On hold"),
+    DRAFT("Draft"),
+    UNDER_REVIEW("Under review"),
+    UNASSIGNED("Unassigned");
+
+    private final String label;
+
+    TaskState(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
